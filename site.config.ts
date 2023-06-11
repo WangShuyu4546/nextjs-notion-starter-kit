60212d28ce76e81008d97392549c231768220cd5
wsy <116859086+WangShuyu4546@users.noreply.github.com>
2023-06-11 23:10:07 +0800

    id

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b04232ab0b3e44e38735bb5fedfc37c6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'healthylifeteam',
+  domain: 'healthylifeteam.vercel.app',
+  author: 'shuyu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
